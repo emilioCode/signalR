f9a986d35b5ce4d05b6459b0234fff895906ce38
Manuel E Mendez <emilio_mem@hotmail.com>
2019-06-02 00:05:37 -0400

    publishing

---
+++ signalR/Hubs/chatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signalR.Hubs
+{
+    public class chatHub  :Hub
+    {
+        public async Task sendMessage(string user, string message,string host)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message, host);
+        }
+
+    }
+}
---
+++ signalR/wwwroot/js/chat.js
@@ -0,0 +1,49 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl('/chatHub').build();
+
+
+connection.on('ReceiveMessage', (user, message, host) => {
+    if (document.getElementById('host').value === host) {
+        var fecha = new Date().toLocaleTimeString();
+        const msg = " <strong> " + user + ": </strong><span style='float:right;'>"+fecha+"</span> <br/>" + message;
+
+        const li = document.createElement('li');
+        li.innerHTML = msg;
+        li.classList.add("list-group-item");
+        document.getElementById('messageList').appendChild(li);
+
+        document.getElementById('mailBox').scrollBy(0, 3000);
+    }
+    
+})
+
+connection.start().catch(err => console.error(err.toString()));
+
+document.getElementById('sendButton').addEventListener('click', event => {
+    const host = document.getElementById('host').value;
+    const user = document.getElementById('user').value;
+    const message = document.getElementById('message').value;
+    if (user != '' && message != '') {
+        connection.invoke('sendMessage', user, message, host).catch(err => console.error(err));
+        document.getElementById('message').value='';
+    }
+    
+    
+    event.preventDefault();
+
+})
+
+document.getElementById('message').addEventListener('keypress', event => {
+    
+    if (event.key === 'Enter') {
+        const host = document.getElementById('host').value;
+        const user = document.getElementById('user').value;
+        const message = document.getElementById('message').value;
+        if (user != '' && message != '') {
+            connection.invoke('sendMessage', user, message, host).catch(err => console.error(err));
+            
+            document.getElementById('message').value = message.substring(0, message.length- message.length - 1);
+        }
+    }
+    
+})
